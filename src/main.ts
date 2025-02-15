6fb4059cca050428fb9c21fc7fc094b2fb6b57c5
ALinYiLin <scliuyilin@163.com>
2025-02-15 17:37:30 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'virtual:svg-icons-register';
+
+const app = createApp(App)
+
+app.mount('#app')
